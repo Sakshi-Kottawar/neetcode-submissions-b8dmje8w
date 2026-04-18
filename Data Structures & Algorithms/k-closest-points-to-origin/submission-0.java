75b1c18bc4fd0043f1cbb35e455fd8c0a54412b0
Sakshi-Kottawar <61192020+Sakshi-Kottawar@users.noreply.github.com>
2026-04-17 19:52:55 -0700

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Pair> maxHeap=new PriorityQueue<>(new Comparator<Pair>() {
+            public int compare(Pair a, Pair b) {
+                return Integer.compare(b.dist, a.dist);
+            }
+        });
+        for(int i=0;i<points.length;i++){
+            int x=points[i][0];
+            int y=points[i][1];
+            int dist=x*x+y*y;
+            maxHeap.add(new Pair(dist,new int[]{x,y}));
+            if(maxHeap.size()>k){
+                maxHeap.poll();
+            }
+        }
+        int[][] res=new int[k][2];
+        int i=0;
+        while(maxHeap.size()>0){
+            Pair p=maxHeap.poll();
+            res[i]=p.coords;
+            i++;
+        }
+        return res;
+        
+
+    }
+}
+class Pair{
+    int dist;
+    int[] coords=new int[2];
+
+    Pair(int dist,int[] coords){
+        this.dist=dist;
+        this.coords=coords;
+    }
+
+}
