e485d71725456d4887dc4f3314827a15f30dd355
Sakshi-Kottawar <61192020+Sakshi-Kottawar@users.noreply.github.com>
2026-05-08 17:41:31 -0700

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj=new ArrayList<>();
+        int n=numCourses;
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<prerequisites.length;i++){
+            int u=prerequisites[i][1];
+            int v=prerequisites[i][0];
+
+            adj.get(u).add(v);
+        }
+        int[] indegree=new int[n];
+
+
+        for(int i=0;i<n;i++){
+            for(int j:adj.get(i)){
+                indegree[j]++;
+            }
+        }
+
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        int res[] =new int[n];
+        int j=0;
+        while(!q.isEmpty()){
+            int node=q.poll();
+            res[j++]=node;
+            for(int i:adj.get(node)){
+                indegree[i]--;
+                
+                if(indegree[i]==0)q.add(i);
+            }
+
+        }
+
+        return j==n?res:new int[0];
+
+    }
+}
