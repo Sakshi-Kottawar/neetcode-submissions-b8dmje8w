096809141588fbe22073b0604ae39004e5080d05
Sakshi-Kottawar <61192020+Sakshi-Kottawar@users.noreply.github.com>
2026-06-03 17:28:38 -0700

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n=edges.length;
+        DSU dsu=new DSU(n+1);
+
+        for(int i=0;i<n;i++){
+            int x=edges[i][0];
+            int y=edges[i][1];
+            if(dsu.find(x)!=dsu.find(y)){
+                dsu.unionBySize(x,y);
+            }else{
+                return new int[]{x,y};
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
+class DSU{
+    int components;
+    int[] parent;
+    int[]size;
+
+    DSU(int n){
+        this.components=n;
+        this.parent=new int[n];
+        this.size=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+
+    public int find(int x){
+        if(x==this.parent[x])return x;
+        return this.parent[x]=find(this.parent[x]);
+    }
+
+    public void unionBySize(int X,int Y){
+        int rootX=find(X);
+        int rootY=find(Y);
+
+        if(this.parent[rootX]==this.parent[rootY]){
+            return;
+        }else if(size[rootX]<=size[rootY]){
+            this.parent[rootX]=rootY;
+            this.size[rootY]+=this.size[rootX];
+        }else{
+            this.parent[rootY]=rootX;
+            this.size[rootX]+=size[rootY];
+        }
+        this.components--;
+    }
+}
