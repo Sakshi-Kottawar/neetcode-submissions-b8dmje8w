b1a1f68b05a1743c4ebfb957d61e79471390324b
Sakshi-Kottawar <61192020+Sakshi-Kottawar@users.noreply.github.com>
2026-06-06 16:57:47 -0700

    Add: count-paths - submission-3

---
+++ Data Structures & Algorithms/count-paths/submission-3.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp=new int[m][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1); 
+        }
+
+        // compute(m-1,n-1,dp);
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0)
+                    dp[i][j]=1;
+                else{
+                    int up=0;
+                    if(i>0)
+                        up=dp[i-1][j];
+                    int left=0;
+                    if(j>0)
+                        left=dp[i][j-1];
+
+                    dp[i][j]= up+left;
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+
+    }
+    private int compute(int m,int n,int[][] dp){
+        if(m==0 && n==0)return dp[m][n]=1;
+        if(m<0 || n<0)return 0;
+        if(dp[m][n]!=-1)return dp[m][n];
+
+
+        int up=compute(m-1,n,dp);
+        int left=compute(m,n-1,dp);
+
+        return dp[m][n]= up+left;
+    }
+}
