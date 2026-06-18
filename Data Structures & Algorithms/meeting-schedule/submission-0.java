798c1fd97deb967e4ed13d055b7449103acc7dff
Sakshi-Kottawar <61192020+Sakshi-Kottawar@users.noreply.github.com>
2026-06-17 20:22:02 -0700

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.java
@@ -0,0 +1,34 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        int n=intervals.size();
+        if(n==0 || n==1)return true;
+        boolean canAttend=true;
+        Collections.sort(intervals,((a,b)->{
+            if(a.start==b.start){
+                return Integer.compare(a.end,b.end);
+
+            }
+            return Integer.compare(a.start,b.start);
+        }));
+        for(int i=1;i<n;i++){
+            Interval prev=intervals.get(i-1);
+            Interval curr=intervals.get(i);
+            if(prev.end>curr.start){
+                canAttend=false;
+                break;
+            }
+        }
+        return canAttend;
+    }
+}
