adbf567c77d9b944a87f50b6a6151c275da6f95d
Sakshi-Kottawar <61192020+Sakshi-Kottawar@users.noreply.github.com>
2026-07-05 21:17:40 -0700

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m=board.length;
+        int n=board[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==word.charAt(0)){
+                    if(solve(board,word,i,j,0)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean solve(char[][] board, String word,int i,int j,int index){
+        int m=board.length;
+        int n=board[0].length;
+        if(index==word.length()){
+            return true;
+        }
+
+
+        if(i<0 || i>=m || j<0 || j>=n ||  board[i][j] != word.charAt(index)){
+            return false;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        boolean found=solve(board,word,i,j+1,index+1)||
+                        solve(board,word,i,j-1,index+1)||
+                         solve(board,word,i+1,j,index+1)||
+                          solve(board,word,i-1,j,index+1);
+        board[i][j]=temp;
+
+        return found;
+        
+
+    }
+}
