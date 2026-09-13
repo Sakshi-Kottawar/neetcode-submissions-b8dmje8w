325252ca8065512ba898d23df12b8606f103b01f
Sakshi-Kottawar <61192020+Sakshi-Kottawar@users.noreply.github.com>
2026-09-13 10:48:39 -0700

    Add: open-the-lock - submission-0

---
+++ Data Structures & Algorithms/open-the-lock/submission-0.java
@@ -0,0 +1,68 @@
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        Queue<String> dq=new LinkedList<>();
+        Set<String> hs=new HashSet<>();
+        hs.add(target);
+        dq.add(target);
+        int level=0;
+
+        while(!dq.isEmpty()){
+            int size=dq.size();
+            for(int i=0;i<size;i++){
+
+                String curr=dq.poll();
+
+                if(curr.equals("0000"))return level; 
+
+                //generate all 8 combinations
+                List<String> previousStates=getAllPossiblePreviousStates(curr,deadends,hs);
+                // System.out.println("current state "+curr);
+                // System.out.println("previous state "+previousStates);
+
+                for(int j=0;j<previousStates.size();j++){
+                    dq.add(previousStates.get(j));
+                    hs.add(previousStates.get(j));
+                }
+                
+            }
+            level++;
+        }
+        return -1;
+    }
+
+    private   List<String> getAllPossiblePreviousStates(String str,String[] deadends,Set<String> hs){
+
+        StringBuilder sb=new StringBuilder(str);
+        Set<String> deadSet = new HashSet<>(Arrays.asList(deadends));
+
+        List<String> res=new ArrayList<>();
+
+
+        for(int i=0;i<sb.length();i++){
+            char originalChar = sb.charAt(i);
+            int digit = originalChar - '0'; 
+            int upDigit=(digit+1)%10;
+            sb.setCharAt(i, (char) (upDigit + '0'));
+            String neighborUp = sb.toString(); 
+            if(!hs.contains(neighborUp) && !deadSet.contains(neighborUp))
+                res.add(neighborUp);
+
+
+            int downDigit=(digit-1)%10;
+            sb.setCharAt(i, (char) (downDigit + '0'));
+            String neighborDown = sb.toString(); 
+
+            if(!hs.contains(neighborDown) && !deadSet.contains(neighborDown))
+                res.add(neighborDown);
+            
+            hs.add(neighborUp);
+            hs.add(neighborDown);
+            sb.setCharAt(i,originalChar);
+
+
+        }
+
+        return res;
+
+    }
+}
